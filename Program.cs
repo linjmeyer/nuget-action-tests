8edc0269947416ba7e1ed75325c5d0a1246cb9b2
Lin Meyer <lin.meyer@redbox.com>
2019-10-23 16:36:00 -0500

    Added everything

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace nuget_action_tests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
